956df74dc128387c631ff19d142e7cb0f0330702
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-08-06 19:28:54 +0200

    Updates

---
+++ precache-manifest.8e476db07cb81a767827dae55415bad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bcafabcabccbf62cf3128fddc46b987",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d9864d98a21b3441600",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "2bb43dc78ac9a4232353",
+    "url": "./static/js/2.4d7d86a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4d7d86a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9864d98a21b3441600",
+    "url": "./static/js/main.18cb7d90.chunk.js"
+  },
+  {
+    "revision": "17c170f40c9fe332cf59",
+    "url": "./static/js/runtime-main.89a40a56.js"
+  }
+]);
