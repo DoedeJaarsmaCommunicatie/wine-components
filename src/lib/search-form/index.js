012abf333b3602c14247abb6f7c3d1e6534c832d
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-08-07 14:44:03 +0200

    fix non-existent import
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- src/lib/search-form/index.js
+++ src/lib/search-form/index.js
@@ -1,6 +1,5 @@
 import { Form } from './Form';
 import { AutoFill } from './AutoFill';
 import { AutoFillItem } from './AutoFillItem';
-import { ThemeProvider as FormThemeProvider } from "./style";
 
-export { Form, AutoFill, AutoFillItem, FormThemeProvider };
+export { Form, AutoFill, AutoFillItem };
